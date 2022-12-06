0377992d3c90d1c62c3ae7265847559c9f48a389
LucianaNascimento <luci.nasrika@hotmail.com>
2022-12-05 22:05:12 -0300

    ajustes na pagina de presenças

---
+++ backend/reuniao_dados.php
@@ -0,0 +1,24 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+// conecta o banco de dados
+$banco = new PDO('mysql:host=localhost;dbname=vocatus', 'root', '',
+    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+$id = 0;
+if(isset($_REQUEST["id"])) $id = $_REQUEST["id"];
+
+
+$sql = "SELECT * from reuniao WHERE reuniao_id = ?";
+
+// passa os dados (parametros) para o SELECT
+$comando = $banco->prepare($sql);
+
+
+$comando->execute(array($id));
+
+$resposta["dados"] = $comando->fetchAll();
+
+
+echo json_encode($resposta);
